90d6a2f13293f4f3abffdee7ab55597ba038108c
Nenyebaby@24 <udochinenye16@gmail.com>
2025-12-24 17:59:35 +0100

    Add .env and update Supabase config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,6 +4,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
+  "https://cskxrqbnewtdiblysbpx.supabase.co",
+  "sb_publishable_iNuaNoswI-e2i-E_wlqLQA_m8Rxm9AX"
 );
